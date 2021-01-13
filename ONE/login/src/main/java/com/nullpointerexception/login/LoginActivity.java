1d0645ed20a954b9c60e2aeff159813bb3ee0429
NIMING <924150590@qq.com>
2021-01-13 23:22:41 +0800

    loginUI

--- ONE/login/src/main/java/com/nullpointerexception/login/LoginActivity.java
+++ ONE/login/src/main/java/com/nullpointerexception/login/LoginActivity.java
@@ -2,13 +2,29 @@ package com.nullpointerexception.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.main_login);
+
+        // 状态栏透明|字体为黑
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
     }
 }
