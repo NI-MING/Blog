cff1486a48035fb909620f0fa786a9a2349bdd0e
NIMING <924150590@qq.com>
2021-01-14 23:01:18 +0800

    登录组件UI与部分逻辑

--- ONE/login/src/main/java/com/nullpointerexception/login/LoginActivity.java
+++ ONE/login/src/main/java/com/nullpointerexception/login/LoginActivity.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText inputPhone;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_login);
+        init();
 
+
+    }
+
+    private void init() {
         // 状态栏透明|字体为黑
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
@@ -25,6 +37,21 @@ public class LoginActivity extends AppCompatActivity {
             getWindow().setNavigationBarColor(Color.TRANSPARENT);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-
+        inputPhone = findViewById(R.id.input_phone);
+        btn_login = findViewById(R.id.btn_login);
+        inputPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(inputPhone.getText().length()!=0){
+                    btn_login.setEnabled(true);
+                }else{
+                    btn_login.setEnabled(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 }
